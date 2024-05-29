3dd3bfb4b2832af526c639700445ce9d7cebf873
mikaislazy <syouqq@gmail.com>
2024-05-29 22:45:43 +0800

    keypoint

--- keypoints.py
+++ keypoints.py
@@ -4,6 +4,7 @@ import mediapipe as mp
 import numpy as np
 import pandas as pd
 import csv
+import json
 
 # Initialize MediaPipe Hands
 mp_hands = mp.solutions.hands
@@ -29,8 +30,10 @@ def extract_hand_keypoints(image):
 
     return hand_keypoints
 
-def get_hand_landmarks_keypoints_dataset(category, main_dataset_folder, sub_dataset_folder, csv_path):
+def get_hand_landmarks_keypoints_dataset(category, main_dataset_folder, sub_dataset_folder, csv_path, json_filename):
+    # Create the CSV file
     for gesture in category:
+        all_gesture_keypoints = []
         for sub in sub_dataset_folder:
             gesture_sub_path = os.path.join(main_dataset_folder, sub, gesture)
             print(gesture_sub_path)
@@ -43,15 +46,24 @@ def get_hand_landmarks_keypoints_dataset(category, main_dataset_folder, sub_data
 
                     # Extract hand keypoints from the image
                     hand_keypoints = extract_hand_keypoints(image)
+                    all_gesture_keypoints.append(hand_keypoints)
 
                     # Save hand keypoints and label to the dataset
                     
                     with open(csv_path, 'a', newline="") as f:
                         writer = csv.writer(f)
                         for h in hand_keypoints:
-                            writer.writerow([gesture, list(h)])
-        else:
-            print("{} not exist".format(gesture_sub_path))
+                            for x, y, z in h:
+                                writer.writerow([gesture, x, y, z])
+                else:
+                    print("File not exist")
+        # calculate the normalised hand keypoints
+        normalized_keypoints = np.array([[landmark.x, landmark.y, landmark.z] for landmark in all_gesture_keypoints])
+        fp = json_filename + '.json'
+        with open(fp, 'a', encoding='UTF-8') as f:
+            json.dump({gesture: normalized_keypoints.tolist()}, f,ensure_ascii=False)
+        
+            
         print("Finish processing hand keypoints Dataset")
 
 if __name__ == '__main__':
@@ -69,5 +81,6 @@ if __name__ == '__main__':
             "Zai",
             "Zen",
             "ZhiJiXiangYin"]
-    csv_path = 'keypoints.csv'
-    get_hand_landmarks_keypoints_dataset(category, main_dataset_folder, sub_dataset_folder, csv_path)
+    csv_path = 'keypoints2.csv'
+    json_filename = 'keypoints2'
+    get_hand_landmarks_keypoints_dataset(category, main_dataset_folder, sub_dataset_folder, csv_path, json_filename)
